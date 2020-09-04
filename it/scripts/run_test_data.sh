b041c734a872771992884c07310a76daef511156
Daniel Sobral <dcsobral@gmail.com>
2020-09-04 15:58:19 -0600

    Use docker node instead of docker container when loading data

--- it/scripts/run_test_data.sh
+++ it/scripts/run_test_data.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-docker container ps
+docker node ls
+docker node ps $(docker node ls -q)
 
-for id in $(docker container ps -f "name=teststack_kafka" --format "{{.ID}}"); do
+for id in $(docker node ps -f "name=teststack_kafka" -q $(docker node ls -q)); do
   echo "Loading data on container $id"
   docker exec -it $id /bin/bash /run/secrets/test_data.sh
 done
