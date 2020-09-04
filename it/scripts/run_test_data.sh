1862d52b56a821279b1bb5d8f048eac3236921eb
Daniel Sobral <dcsobral@gmail.com>
2020-09-04 16:46:28 -0600

    Test with two different alternatives to docker ps
    Ultimately it might be needed to run docker-machine env, but since
    that's not available on the docker for mac I'd rather avoid it.

--- it/scripts/run_test_data.sh
+++ it/scripts/run_test_data.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
 
-docker ps -f name=teststack_kafka
+echo "Docker stack ps"
+docker stack ps teststack
 
-for id in $(docker ps -f "name=teststack_kafka" -q); do
-  echo "Loading data on container $id"
-  docker exec -it $id /bin/bash /run/secrets/test_data.sh
+for id in $(docker stack ps teststack -f 'name=teststack_kafka' -q); do
+  echo "Loading data on node $id"
+done
+
+echo "Docker service ps"
+docker service ps
+
+for service in kafka_ssh kafka_local; do
+  echo "Service $service"
+  id="$(docker service ps -f "name=teststack_$service.1" "teststack_$service" --no-trunc -q | head -n1)"
+  echo "Loading data on container teststack_$service.1.$id"
+  docker exec -it "teststack_$service.1.$id" /bin/bash /run/secrets/test_data.sh
 done
